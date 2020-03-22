b13b924fa1d89c90a1dc9d9010d122401e299103
edvaldoperreira <edvaldo.mail@gmail.com>
2020-03-22 16:48:18 +0000

    Refactoring and DAO class implement

--- app/routes/article.js
+++ app/routes/article.js
@@ -1,14 +1,26 @@
 module.exports = function (app) {
-	app.get('/article', function(req, res) {
+
+	app.get('/article', function (req, res) {
+		const connection = app.config.dbConnection();
+		const dao = new app.app.models.ArticleDAO(connection);
+
+		dao.getArticle(function (error, result) {
+			res.render('article/article', { article: result });
+		});
+	});
+
+	app.post('/article', function (req, res) {
+		const article = req.body;
 
 		const connection = app.config.dbConnection();
 		const dao = new app.app.models.ArticleDAO(connection);
 
-		dao.getArticle(function(error, result) {
-			if (error) {
-				res.send(error);
-			} else {
-				res.render('article', {article: result});
-			}});
+		dao.addArticle(article, function (error, result) {
+			res.redirect('/');
+		});
 	});
-}
+
+	app.get('/article/new', function (req, res) {
+		res.render('article/form-article');
+	});
+};
